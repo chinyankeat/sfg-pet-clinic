25f9ec14a21b98fc8b59cdbd8c910f3e6919b2de
Chin Yan Keat <chinyankeat@yahoo.com>
2020-09-08 01:01:34 +0800

    Initial Project Add

---
+++ src/test/java/guru/chinyankeat/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.chinyankeat.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
